1962b73c1b1b91b9ee0d105819ed0344d153af53
Pablo Grant <grantpablo4@gmail.com>
2021-08-10 04:15:28 -0500

    add test

---
+++ src/app/form.directive.spec.ts
@@ -0,0 +1,10 @@
+import { ViewContainerRef } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+import { FormDirective } from './form.directive';
+
+describe('FormDirective', () => {
+  it('should create an instance', () => {
+    
+    // expect(directive).toBeTruthy();
+  });
+});
