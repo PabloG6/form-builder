9b6473de3bafc05b36d83b0e51d89a4004fa52d4
Pablo Grant <grantpablo4@gmail.com>
2021-08-10 04:16:28 -0500

    app: committing scaffold for app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
+import { Platform } from '@angular/cdk/platform';
+import { ComponentFactoryResolver, OnInit, ViewChild } from '@angular/core';
 import { Component } from '@angular/core';
-
+import { FormControl, NgControl } from '@angular/forms';
+import { MatFormField, MatFormFieldControl, MatFormFieldModule } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import { Observable, of } from 'rxjs';
+import { FormDirective } from './form.directive';
+import { MatInputWrapperComponent } from './mat-input-wrapper/mat-input-wrapper.component';
+import { TextFieldUnion } from './types/form-field.type';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'form-builder';
+export class AppComponent implements OnInit {
+    
+  
+  constructor(private componentFactoryResolver: ComponentFactoryResolver) {
+    
+    
+
+  }
+  ngOnInit(): void {
+    
+    
 }
+
+  
+}
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormDirective } from './form.directive';
+import { MatInputWrapperComponent } from './mat-input-wrapper/mat-input-wrapper.component';
+import {MatRadioModule} from '@angular/material/radio';
+import { RadioWrapperComponent } from './radio-wrapper/radio-wrapper.component';
+import { SelectWrapperComponent } from './select-wrapper/select-wrapper.component';
+import {MatSelectModule} from '@angular/material/select';
+import { CheckboxWrapperComponent } from './checkbox-wrapper/checkbox-wrapper.component';
+import { DateWrapperComponent } from './date-wrapper/date-wrapper.component';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
